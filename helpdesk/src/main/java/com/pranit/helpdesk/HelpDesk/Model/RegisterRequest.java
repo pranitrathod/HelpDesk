2f3beb0f3fbc3a55bce83524f3683c5f4a79a811
Pranit Rathod <77391148+pranitrathod@users.noreply.github.com>
2026-06-06 14:42:36 +0530

    New feature added

---
+++ helpdesk/src/main/java/com/pranit/helpdesk/HelpDesk/Model/RegisterRequest.java
@@ -0,0 +1,25 @@
+
+package com.crh.dto;
+
+import com.crh.entity.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    @Email(message = "Invalid email")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    private Role role;
+}
