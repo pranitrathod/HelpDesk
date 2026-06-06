62ebf772d27e0d4e5fc4a072d8efa0840ead9e73
Pranit Rathod <77391148+pranitrathod@users.noreply.github.com>
2026-06-06 14:17:55 +0530

    Add files via upload

---
+++ helpdesk/src/main/java/com/pranit/helpdesk/HelpDesk/Entity/ComplaintStatus.java
@@ -0,0 +1,12 @@
+package com.crh.entity;
+
+public enum ComplaintStatus {
+    CREATED,
+    UNDER_REVIEW,
+    VENDOR_CONTACTED,
+    INVESTIGATION,
+    APPROVED,
+    REFUND_PROCESSED,
+    REPLACEMENT_SHIPPED,
+    CLOSED
+}
