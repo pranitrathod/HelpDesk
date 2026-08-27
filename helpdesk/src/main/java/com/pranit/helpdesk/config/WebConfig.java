32af25e1a1b4c66ec7bc5be1cc512ed7071ac193
Pranit Rathod <77391148+pranitrathod@users.noreply.github.com>
2026-08-27 22:46:25 +0530

    feat: configure API CORS

---
+++ helpdesk/src/main/java/com/pranit/helpdesk/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.pranit.helpdesk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
